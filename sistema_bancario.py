88afa8f464d58cdd0c6902fed174060d73f208c6
Marcio-Balivo <marcio_balivo@yahoo.com.br>
2024-10-06 23:47:43 -0300

    feature: add depositar, sacar e emitir extrato

---
+++ sistema_bancario.py
@@ -0,0 +1,69 @@
+'''
+Obj: criar um sistema bancário com as funções sacar, depositar e ver extrato.
+'''
+
+COMPRIMENTO = 80
+SAQUE_MAX = 500.0
+saldo = 0.0
+transacoes = 0
+extrato = ""
+
+menu = f'''\
+{"".center(COMPRIMENTO, "=")}
+{" SISTEMA BANCÁRIO ".center(COMPRIMENTO, "$")}
+{"".center(COMPRIMENTO, "=")}
+OPÇÕES:
+1. Depositar
+2. Sacar
+3. Emitir Extrato
+
+0. Sair
+{"".center(COMPRIMENTO, "=")}'''
+
+while True:
+    print(menu)
+    opcao = input("Informe a opção desejada: ")
+
+    if opcao == "0":
+        exit (0)
+    elif opcao == "1":
+        cabecalho_depositar = f'''
+{"".center(COMPRIMENTO, "=")}
+{" DEPOSITAR ".center(COMPRIMENTO, "+")}
+{"".center(COMPRIMENTO, "=")}'''
+        print(cabecalho_depositar)
+        valor = float(input("Informe o valor a depositar: R$ "))
+        if valor <= 0:
+            print("\nO valor deve ser maior que 0(zero)!\n")
+        elif transacoes == 10:
+            print("\nLimite de transações diárias (10) atingido!\n")
+        else:
+            saldo += valor
+            transacoes += 1
+            extrato += f"Depósito R$ {valor:.2f}\n"
+            print("\nO depósito foi realizado!\n")
+    elif opcao == "2":
+        cabecalho_sacar = f'''
+{"".center(COMPRIMENTO, "=")}
+{" SACAR ".center(COMPRIMENTO, "-")}
+{"".center(COMPRIMENTO, "=")}'''
+        print(cabecalho_sacar)
+        valor = float(input("Informe o valor a sacar: R$ "))
+        if valor > SAQUE_MAX:
+            print("\nO valor deve ser menor ou igual a R$ 500,00!\n")
+        elif valor > saldo:
+            print("\nO saldo disponível é insuficiente!\n")
+        elif transacoes == 10:
+            print("\nLimite de transações diárias (10) atingido!\n")
+        else:
+            saldo -= valor
+            transacoes += 1
+            extrato += f"Saque R$ {valor:.2f}\n"
+            print("\nO saque foi realizado!\n")
+    elif opcao == "3":
+        cabecalho_extrato = f'''
+{"".center(COMPRIMENTO, "=")}
+{" EXTRATO ".center(COMPRIMENTO, "!")}
+{"".center(COMPRIMENTO, "=")}'''
+        print(cabecalho_extrato)
+        print(f"{extrato}{''.center(COMPRIMENTO, '=')}\nSaldo R$ {saldo:.2f}\n") if extrato else print("\nNenhuma operação foi registrada!\n")
